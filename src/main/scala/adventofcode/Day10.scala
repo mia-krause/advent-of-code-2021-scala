80131e15ad8362f1eafdd080e0fb3f2a3c80b2c6
mia-krause <mia.elisabeth.krause+github@gmail.com>
2021-12-15 22:00:10 +0100

    Day10

--- src/main/scala/adventofcode/Day10.scala
+++ src/main/scala/adventofcode/Day10.scala
@@ -1,21 +1,44 @@
 package adventofcode
 
+import scala.collection.mutable
 import scala.io.Source
 
 object Day10 {
   val day = 10
-  val example = true
+  val example = false
 
-  enum BracketType {
-    case ROUND, SQUARE, CURLY, ANGLE
+  enum BracketType(val weight: Int) {
+    case ROUND extends BracketType(1)
+    case SQUARE extends BracketType(2)
+    case CURLY extends BracketType(3)
+    case ANGLE extends BracketType(4)
   }
 
   enum BracketFunction {
     case OPENING, CLOSING
   }
 
+  def main(args: Array[String]): Unit = {
+    val input = readFile()
+    val completionStrings = input.map(findCompletionString(_)).filter(!_.isEmpty)
+    completionStrings.foreach(println(_))
+    val evaluatedCompletionStrings = completionStrings.map(evaluateCompletionString(_)).sorted
+
+    println(evaluatedCompletionStrings((evaluatedCompletionStrings.length/2).toInt))
+  }
+
   class Bracket(val typ: BracketType, val function: BracketFunction) {
-    override def toString: String = Bracket.mapping.filter((_, bracket) => this.equals(bracket)).keys.head.toString
+    override def toString: String = {
+      val filteredMapping = Bracket.mapping.filter((_, bracket: Bracket) => this.equals(bracket)).keys
+        filteredMapping.head.toString
+    }
+
+    override def equals(obj: Any): Boolean = obj match {
+      case Bracket(t,f) => typ==t && function==f
+      case _ => false
+    }
+
+    override def hashCode(): Int = typ.weight*function.ordinal
   }
 
   object Bracket {
@@ -32,18 +55,50 @@ object Day10 {
 
     def apply(c: Char) = mapping.get(c).get
 
+    def unapply(bracket: Bracket) = (bracket.typ, bracket.function)
+
   }
 
-  def main(args: Array[String]): Unit = {
-    val input = readFile()
-  }
-
-  def readFile(): List[Bracket] = {
+  def readFile(): List[List[Bracket]] = {
     var filename = s"src/main/resources/input_day${day}"
     if (example) filename += "_example"
     val source = Source.fromFile(filename)
-    val input = source.getLines.mkString
+    val input = source.getLines.toList
     source.close()
-    input.toCharArray.map(Bracket(_)).toList
+    input.map(_.toCharArray.map(Bracket(_)).toList).toList
+  }
+
+  def findCorruptedBrackets(brackets: List[Bracket]): List[Bracket] = {
+    val unclosedBrackets = mutable.Stack[BracketType]()
+    var corruptedBrackets = List[Bracket]()
+    for(bracket <- brackets) {
+      bracket match {
+        case Bracket(_, BracketFunction.OPENING) => unclosedBrackets.push(bracket.typ)
+        case Bracket(t, BracketFunction.CLOSING) => {
+          val lastUnclosedBracket = unclosedBrackets.pop()
+          if(lastUnclosedBracket!=t) corruptedBrackets = corruptedBrackets.appended(bracket)
+        }
+      }
+    }
+    corruptedBrackets
+  }
+
+  def findCompletionString(brackets: List[Bracket]) : List[BracketType] = {
+    val unclosedBrackets = mutable.Stack[BracketType]()
+    for(bracket <- brackets) {
+      bracket match {
+        case Bracket(_, BracketFunction.OPENING) => unclosedBrackets.push(bracket.typ)
+        case Bracket(t, BracketFunction.CLOSING) => {
+          val lastUnclosedBracket = unclosedBrackets.pop()
+          if(lastUnclosedBracket!=t) return List()
+        }
+      }
+    }
+    unclosedBrackets.toList
+  }
+
+  def evaluateCompletionString(brackets: List[BracketType]): Long = {
+    var weights = brackets.map(_.weight)
+    weights.foldLeft(0l)((total, nextWeight) => (total*5)+nextWeight)
   }
 }
